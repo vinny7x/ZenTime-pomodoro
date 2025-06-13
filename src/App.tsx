e89157426034664e38f1f76d4dd71b0283f2425c
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-06-13 01:22:54 -0300

    useState error

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,27 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Home } from "./pages/Home";
-import { useState } from "react";
-import type { TaskStateModel } from "./models/TaskStateModel";
+//import { useState } from "react";
+//import type { TaskStateModel } from "./models/TaskStateModel";
 import { TaskContext } from "./contexts/TaskContext";
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakeTime: 5,
-    longBreakeTime: 15,
-  },
-};
+// const initialState: TaskStateModel = {
+//   tasks: [],
+//   secondsRemaining: 0,
+//   formattedSecondsRemaining: "00:00",
+//   activeTask: null,
+//   currentCycle: 0,
+//   config: {
+//     workTime: 25,
+//     shortBreakeTime: 5,
+//     longBreakeTime: 15,
+//   },
+// };
 
 export function App() {
   //const [state, setState] = useState(initialState);
   return (
-    <TaskContext.Provider value={{}}>
+    <TaskContext.Provider value={{chave:"df"}}>
       <Home />
     </TaskContext.Provider>
   );
