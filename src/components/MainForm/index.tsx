2f3dc5ca30ec71ad5b2dad10bd21b4b8329495bc
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-06-22 22:33:18 -0300

    input controlado (value e onChange)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,26 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles/indes";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useState } from "react";
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('')
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    console.log('foi');
+    
+
+  }
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <DefaultInput
           labelText="task"
           id="meuInput"
           type="text"
           placeholder="Digite algo"
+          value={taskName}
+          onChange={(e)=>setTaskName(e.target.value)}
         />
       </div>
       <div className="formRow">
