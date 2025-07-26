6b8501b895bf06d7577af5151aac2764fefb43f0
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-07-25 21:51:43 -0300

    fix: taskActionsType
    um erro em que empedia o enum de ser utilizado foi corrigido, utilizando o 'as const' e o typeof

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -7,33 +7,36 @@
 
 import type { TaskModel } from "../../models/TaskModel";
 
-export enum TaskActionTypes {
-  START_TASK = "START_TASK",
-  INTERRUPT_TASK = "INTERRUPT_TASK",
-  RESET_STATE = "RESET_STATE",
-  COUNT_DOWN = "COUNT_DOWN",
-  COMPLETE_TASK = "COMPLETE_TASK",
-}
+export const TaskActionTypes = {
+  START_TASK: "START_TASK",
+  INTERRUPT_TASK: "INTERRUPT_TASK",
+  RESET_STATE: "RESET_STATE",
+  COUNT_DOWN: "COUNT_DOWN",
+  COMPLETE_TASK: "COMPLETE_TASK",
+} as const;
+
+export type TaskActionTypes =
+  (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
 
 export type TaskActionsWithPayload =
   | {
-      type: TaskActionTypes.START_TASK;
+      type: typeof TaskActionTypes.START_TASK;
       payload: TaskModel;
     }
   | {
-      type: TaskActionTypes.COUNT_DOWN;
+      type: typeof TaskActionTypes.COUNT_DOWN;
       payload: { secondsRemaining: number };
     };
 
 export type TaskActionsWithoutPayload =
   | {
-      type: TaskActionTypes.RESET_STATE;
+      type: typeof TaskActionTypes.RESET_STATE;
     }
   | {
-      type: TaskActionTypes.INTERRUPT_TASK;
+      type: typeof TaskActionTypes.INTERRUPT_TASK;
     }
   | {
-      type: TaskActionTypes.COMPLETE_TASK;
+      type: typeof TaskActionTypes.COMPLETE_TASK;
     };
 
 export type TaskActionModel =
